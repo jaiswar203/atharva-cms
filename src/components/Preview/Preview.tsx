6ebbe030c2242a98022b051c4afde710f0566509
Nilesh <jignesg190@gmail.com>
2025-02-05 11:59:03 +0530

    Remove unused className prop from MobilePreview component

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -3,15 +3,15 @@ import 'react-device-frameset/styles/marvel-devices.min.css'
 
 interface MobilePreviewProps {
     children: React.ReactNode
-    className?: string
     device?: 'iPhone X'
     color?: string
     landscape?: boolean
 }
 
-export const MobilePreview = ({ children, className, device = 'iPhone X', color = 'gold', landscape = false }: MobilePreviewProps) => {
+
+export const MobilePreview = ({ children, device = 'iPhone X', color = 'gold', landscape = false }: MobilePreviewProps) => {
     return (
-        <DeviceFrameset device={device} color={color} landscape={landscape} className={className}>
+        <DeviceFrameset device={device} color={color} landscape={landscape} >
             {children}
         </DeviceFrameset>
     )
